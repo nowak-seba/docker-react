eed77bcba68603b66b5c1666aaea47922f35d890
Sebastian Nowak <sebastian.nowak@caspio.com>
2021-02-23 22:07:56 +0100

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,15 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link 3', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 4', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
